d38370b1a455c34081af98becc9ad5a249c11cf7
Sandeep <sandeep@letstranzact.com>
2020-08-16 13:51:34 +0530

    add check for the day of week and add new servers

--- connect.sh
+++ connect.sh
@@ -2,14 +2,21 @@
 
 # Loading the config file with details
 config_path="/home/sandeepk/scripts/servers.json"
-servers=(s1 s2)
+servers=(s1 s2 cmm)
+day=$(date | cut -d " " -f1)
+not_days=(Sun Sat)
+if [[ " ${not_days[@]} " =~ $day ]]
+then
+    exit 1
+fi
+
 # echo $(cat $config_path | jq '.'$1)
 echo $1
 if [[ " ${servers[@]} " =~ $1 ]]; then
     ip=$(cat $config_path | jq -r '.'$1'.ip')
     pem=$(cat $config_path | jq -r '.'$1'.pem')
     name=$(cat $config_path | jq '.'$1'.name')
-    #echo "Logging in to $name ..."
+    #echo "Logging in to $name $ip $pem ..."
     sudo ssh -i $pem ubuntu@$ip
 else
     echo "Wrong args passed"
