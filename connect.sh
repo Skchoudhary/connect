60031fbd43a2b20732f6d61b8ced68546d259a16
Sandeep Choudhary <sandeepchoudhary1507@gmail.com>
2020-12-14 14:19:00 +0530

    add help option in connect utility

--- connect.sh
+++ connect.sh
@@ -1,17 +1,23 @@
 #! /bin/bash
 
-# Loading the config file with details
+# $1 can be one of the given option(s) help, servers keys.
 config_path="./servers.json"
-servers=(s1 s2 cmm)
-# echo $(cat $config_path | jq '.'$1)
-echo $1
+# get all the valid servers keys
+servers=$(cat servers.json | jq 'keys')
+
+# check the args passed is valid server keys or not
 if [[ " ${servers[@]} " =~ $1 ]]; then
     ip=$(cat $config_path | jq -r '.'$1'.ip')
     pem=$(cat $config_path | jq -r '.'$1'.pem')
     name=$(cat $config_path | jq '.'$1'.name')
-    #echo "Logging in to $name $ip $pem ..."
     sudo ssh -i $pem ubuntu@$ip
+elif [ "$1" == "help" ]; then
+    echo "run comand as shown below"
+    echo ">>> ./connect server_keys"
+    echo -e "valid args \n $servers"
 else
     echo "Wrong args passed"
+    echo -e "valid args \n $servers"
 fi
+
 
